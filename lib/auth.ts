a34be35f6f33bee1d12ab4486d8b8a8c0496ea9f
FjrREPO <jossfajar21@gmail.com>
2024-06-11 00:11:40 +0700

    fix auth option

---
+++ lib/auth.ts
@@ -0,0 +1,70 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/lib/prisma";
+import bcrypt from "bcryptjs";
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma) as any,
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            profile(profile) {
+                return {
+                    id: profile.sub,
+                    name: `${profile.given_name} ${profile.family_name}`,
+                    email: profile.email,
+                    image: profile.picture,
+                    role: profile.role ? profile.role : "user",
+                };
+            },
+        }),
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {
+                email: { label: "Email", type: "text" },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) {
+                    throw new Error("Invalid credentials");
+                }
+
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email,
+                    },
+                });
+
+                if (!user || !user?.password) {
+                    throw new Error("Invalid credentials");
+                }
+
+                const isCorrectPassword = await bcrypt.compare(credentials.password, user.password);
+
+                if (!isCorrectPassword) {
+                    throw new Error("Invalid credentials");
+                }
+
+                return user;
+            },
+        }),
+    ],
+    session: {
+        strategy: "jwt",
+    },
+    pages: {
+        signIn: "/signin",
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            return { ...token, ...user };
+        },
+        async session({ session, token }) {
+            session.user.role = token.role;
+            return session;
+        },
+    },
+};
