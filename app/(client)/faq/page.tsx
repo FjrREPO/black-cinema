d02ddc25a3c80e583f478e7262855a51363f6562
FjrREPO <jossfajar21@gmail.com>
2024-06-11 09:08:15 +0700

    fix faq

--- app/(client)/faq/page.tsx
+++ app/(client)/faq/page.tsx
@@ -1,13 +1,16 @@
 "use client"
 import React from "react";
-import { User } from "lucide-react";
 import Faq from "./_components/Faq";
 import Navbar from "@/components/Navbar";
+import getCurrentUser from "@/app/_actions/get-user";
+import { getAllPayment } from "@/app/_actions/get-all-payment";
 
-const FaqPage = () => {
+const FaqPage = async () => {
+    const user = await getCurrentUser()
+    const payment = await getAllPayment()
     return (
         <div>
-            <div><Navbar user={User} /></div>
+            <div><Navbar user={user} payment={payment}/></div>
             <div><Faq /></div>
         </div>
     );
