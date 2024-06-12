39f929596ceae2ca6be67338c7b0fccba0f1625c
FjrREPO <jossfajar21@gmail.com>
2024-06-12 22:22:33 +0700

    fix ui

--- app/(client)/carakerja/[nameCard]/_components/Carakerja.tsx
+++ app/(client)/carakerja/[nameCard]/_components/Carakerja.tsx
@@ -41,7 +41,7 @@ export default function Carakerja({ paymentMethod, isLoading, nameCard }: { paym
                         <tr className="w-full">
                             <td>
                                 <SkeletonWrapper isLoading={isLoading}>
-                                    <Accordion type="single" collapsible className="text-white">
+                                    <Accordion type="single" collapsible className="text-white px-5">
                                         <AccordionItem value="item-1">
                                             <AccordionTrigger>Bayar dengan nomor rekening {filteredMethod?.nameCard}</AccordionTrigger>
                                             <AccordionContent>
@@ -71,7 +71,7 @@ export default function Carakerja({ paymentMethod, isLoading, nameCard }: { paym
                                         Bayar pesanan ke Virtual Account diatas sebelum membuat pesanan
                                         kembali dengan Virtual Account agar nomor tetap sama
                                     </p>
-                                    <div className="flex justify-end mb-4">
+                                    <div className="flex justify-center mb-4">
                                         <button type="button" onClick={handleBackClick} className="text-blue-700 duration-200 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Kembali</button>
                                     </div>
                                 </SkeletonWrapper>
